ecf27f01e5b634b17f857d468f41c8ef03b64134
Jasmine Nguyen <jgnuyen1941@sdsu.edu>
2022-02-05 03:17:53 -0800

    lesgo

--- Assets/Scripts/UI_Controller.cs
+++ Assets/Scripts/UI_Controller.cs
@@ -13,7 +13,6 @@ namespace CS583
     {
         public Text CharNameout;
         public InputField CharNameIn;
-        public Text textOut_Strength;
 
         private Text t_Strength;
         private Button b_Roll_Strength;
@@ -52,6 +51,21 @@ namespace CS583
             b_Roll_Strength = GameObject.Find("b_Roll_Strength").GetComponent<Button>();
             b_Roll_Strength.onClick.AddListener(CallBack_Roll_Strength);
 
+            t_Dexterity = GameObject.Find("t_Dexterity").GetComponent<Text>();
+            b_Roll_Dexterity = GameObject.Find("b_Roll_Dexterity").GetComponent<Button>();
+            b_Roll_Dexterity.onClick.AddListener(CallBack_Roll_Dexterity);
+
+            t_Intelligence = GameObject.Find("t_Intelligence").GetComponent<Text>();
+            b_Roll_Intelligence = GameObject.Find("b_Roll_Intelligence").GetComponent<Button>();
+            b_Roll_Intelligence.onClick.AddListener(CallBack_Roll_Intelligence);
+
+            t_Charisma = GameObject.Find("t_Charisma").GetComponent<Text>();
+            b_Roll_Charisma = GameObject.Find("b_Roll_Charisma").GetComponent<Button>();
+            b_Roll_Charisma.onClick.AddListener(CallBack_Roll_Charisma);
+
+            t_Constitution = GameObject.Find("t_Constitution").GetComponent<Text>();
+            b_Roll_Constitution = GameObject.Find("b_Roll_Constitution").GetComponent<Button>();
+            b_Roll_Constitution.onClick.AddListener(CallBack_Roll_Constitution);
         }
 
         private void CallBack_EditEnd()
@@ -73,6 +87,30 @@ namespace CS583
         {
             int strength = DiceRollAlgo();
             t_Strength.text = strength.ToString();
+
+        }
+        public void CallBack_Roll_Dexterity()
+        {
+            int dexterity = DiceRollAlgo();
+            t_Dexterity.text = dexterity.ToString();
+
+        }
+        public void CallBack_Roll_Intelligence()
+        {
+            int intelligence = DiceRollAlgo();
+            t_Intelligence.text = intelligence.ToString();
+
+        }
+        public void CallBack_Roll_Charisma()
+        {
+            int charisma = DiceRollAlgo();
+            t_Charisma.text = charisma.ToString();
+
+        }
+        public void CallBack_Roll_Constitution()
+        {
+            int constitution = DiceRollAlgo();
+            t_Constitution.text = constitution.ToString();
 
         }
 
